b62c6ef263f71f5046aaaec87e92089ffd2f1119
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-20 20:32:14 -0300

    feat: added interative button 'edit'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Player from "./components/Player";
+import Player from "./components/Player.jsx";
 
 function App() {
 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
+
 export default function Player({ name, symbol }){
+    const [ isEditing, setIsEditing ] = useState( false );
+
+    function handleEditClick(){
+        setIsEditing( true );
+    }
+    let playerName = <span className="player-name">{name}</span> 
+
+    if (isEditing){
+        playerName = <input type='text' required />
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+                {playerName}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={ handleEditClick }>Edit</button>
         </li>
     )
 }
