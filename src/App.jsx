0df8b76a2a46753e9938d953a9a80e73d19aa040
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-20 20:13:36 -0300

    feat: created Player component and new file Player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import Player from "./components/Player";
 
 function App() {
-  
+
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>         
+        </ol>
+
+        GAME BOARD
+
+      </div>
+
+
+
+    </main>
+
+
+
+
+
+
   )
 }
 
