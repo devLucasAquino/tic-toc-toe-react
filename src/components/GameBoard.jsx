21681ee85b33fd6519c049c4258f32db4fead2bf
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-20 22:16:55 -0300

    feat: added useState in game board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initalGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,22 @@ const initalGameBoard = [
 ]
 
 export default function GameBoard(){
+    const [gameBoard, setGameBoard] = useState(initalGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) =>{
+            const uptadedBoard = [...prevGameBoard.map(innerArray => [...innerArray])] 
+            uptadedBoard[rowIndex][colIndex] = 'X';
+            return uptadedBoard;
+        })
+    }
+
     return(
         <ol id="game-board">
-            {initalGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
