6bc2557ba32c7d9747ad85ace2c949c5d4950d85
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-20 21:34:49 -0300

    feat: added function name changed => two-way-binding

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X"/>
-          <Player name="Player 2" symbol="O"/>         
+          <Player initialName="Player 1" symbol="X"/>
+          <Player initialName="Player 2" symbol="O"/>         
         </ol>
 
         GAME BOARD
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,32 @@
 import { useState } from 'react';
 
 
-export default function Player({ name, symbol }){
+export default function Player({ initialName, symbol }){
+    const [ playerName, setPlayerName] = useState(initialName);
     const [ isEditing, setIsEditing ] = useState( false );
 
     function handleEditClick(){
-        setIsEditing( true );
+        setIsEditing( (editing) => !editing );
     }
-    let playerName = <span className="player-name">{name}</span> 
+
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
 
     if (isEditing){
-        playerName = <input type='text' required />
+        editablePlayerName = <input type='text' 
+        required defaultValue={playerName} onChange={handleChange}/>
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                { editablePlayerName }
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={ handleEditClick }>Edit</button>
+            <button onClick={ handleEditClick }>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
